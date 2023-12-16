471d849d783e6922bae70e505c6cf273017a487c
Lior <l.shavitq@gmail.com>
2023-12-16 19:29:48 +0200

    AS1

--- androidstudio/app/src/main/java/com/example/quietheart/MainActivity.java
+++ androidstudio/app/src/main/java/com/example/quietheart/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.quietheart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
