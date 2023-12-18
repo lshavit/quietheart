9a3fcb3a254e622c3e9ec2480503b0f22de8aa57
Lior <l.shavitq@gmail.com>
2023-12-18 14:58:48 +0200

    123

--- androidstudio/app/src/main/java/com/example/quietheart/SecondActivity.java
+++ androidstudio/app/src/main/java/com/example/quietheart/SecondActivity.java
@@ -8,5 +8,5 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2); }
+        setContentView(R.layout.activity_second); }
 }
